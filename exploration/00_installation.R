2a6401b01e9edac940d353f3234e9615c5dc581e
sayadennis <sayarenedennis@gmail.com>
2024-02-13 16:00:51 -0600

    add DNAcopy library to installation script

--- exploration/00_installation.R
+++ exploration/00_installation.R
@@ -1,6 +1,7 @@
-if (!require("BiocManager", quietly = TRUE))
-    install.packages("BiocManager")
+if (!require("BiocManager", quietly = TRUE)) {
+  install.packages("BiocManager")
+}
 
 BiocManager::install("sesame")
 install.packages("pals")
-
+BiocManager::install("DNAcopy")
