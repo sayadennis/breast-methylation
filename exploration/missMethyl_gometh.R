e9713880edea6178e1e1d6bc80d517911453e187
sayadennis <sayarenedennis@gmail.com>
2024-05-20 18:10:13 -0500

    run gometh to study probes driving recurring TFBS hits

--- exploration/missMethyl_gometh.R
+++ exploration/missMethyl_gometh.R
@@ -26,6 +26,10 @@ for (setname in probeset_names) {
   probe_sets[[setname]] <- readLines(file_path)
 }
 
+#############################################
+#### First run gometh for all DML probes ####
+#############################################
+
 gometh_res <- list()
 
 for (setname in probeset_names) {
@@ -33,13 +37,15 @@ for (setname in probeset_names) {
   test_go <- gometh(
     sig.cpg = probe_sets[[setname]],
     # all.cpg=rownames(top),  # TODO: output all CpGs used for DML during DML step!!
-    collection = c("GO", "KEGG"),
+    collection = "GO",
+    array.type = "EPIC",
     plot.bias = FALSE
   )
   test_kegg <- gometh(
     sig.cpg = probe_sets[[setname]],
     # all.cpg=rownames(top),  # TODO: output all CpGs used for DML during DML step!!
     collection = "KEGG",
+    array.type = "EPIC",
     plot.bias = FALSE
   )
   test_go <- test_go[test_go$FDR < 0.05, ]
@@ -53,11 +59,74 @@ for (setname in probeset_names) {
   print(paste("  KEGG:", dim(test_kegg)[1]))
   print("")
   # Write results to disk
-  for (query in c("GO", "KEGG")) {
-    if (dim(gometh_res[[setname]][[query]])[1] > 0) {
+  for (collection in c("GO", "KEGG")) {
+    if (dim(gometh_res[[setname]][[collection]])[1] > 0) {
       write.csv(
-        gometh_res[[setname]][[query]],
-        paste0(dout, "/gometh_", query, "_", setname, ".csv"),
+        gometh_res[[setname]][[collection]],
+        paste0(dout, "/gometh_", collection, "_", setname, ".csv"),
+        row.names = TRUE
+      )
+    }
+  }
+}
+
+######################################################
+#### Next run gometh for probes driving TFBS hits ####
+######################################################
+
+## Download TFBS Database
+db <- KYCG_getDBs("KYCG.EPIC.TFBSconsensus.20211013")
+
+tfs <- c(
+  "ZNF217", "AHR", "TLE3", "GREB1", "GRHL2",
+  "ESR1", "PR", "FOSL2", "AR", "GATA3",
+  "NR3C1", "HES2"
+)
+
+# Get the probe set names
+left_name <- "hypo_refCFN_compCUB"
+right_name <- "hypo_refAN_compTU"
+
+# Get all probes in comparisons
+left_probes <- probe_sets[[left_name]]
+right_probes <- probe_sets[[right_name]]
+
+# List of probes in the left- and right-comaprison DML sets
+left_probes_tf_all <- c()
+right_probes_tf_all <- c()
+
+for (tf in tfs) {
+  # Get probes that overlap with TFBS
+  tf_probes <- db[[tf]]
+  left_probes_tf <- intersect(left_probes, tf_probes)
+  right_probes_tf <- intersect(right_probes, tf_probes)
+
+  # Add to left and right probe sets that overlap with TFBS
+  left_probes_tf_all <- union(left_probes_tf, left_probes_tf_all)
+  right_probes_tf_all <- union(right_probes_tf, right_probes_tf_all)
+}
+
+# Get left- and right-only probes
+tf_probes <- list(
+  "TFBS_CFN_vs_CUB_only" = setdiff(left_probes_tf_all, right_probes_tf_all),
+  "TFBS_AN_vs_TU_only" = setdiff(right_probes_tf_all, left_probes_tf_all),
+  "TFBS_CFNvCUB_ANvTU_overlap" = intersect(left_probes_tf_all, right_probes_tf_all)
+)
+
+for (setname in names(tf_probes)) {
+  for (collection in c("GO", "KEGG")) {
+    # Run gometh enrichment analysis
+    test <- gometh(
+      sig.cpg = tf_probes[[setname]],
+      collection = collection,
+      array.type = "EPIC",
+      plot.bias = FALSE
+    )
+    test <- test[test$P.DE < 0.05, ] # cut with nominal p-value in case no FDR hits
+    if (dim(test)[1] > 0) {
+      write.csv(
+        test,
+        paste0(dout, "/gometh_", collection, "_", setname, ".csv"),
         row.names = TRUE
       )
     }
