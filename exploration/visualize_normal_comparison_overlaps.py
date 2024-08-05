4d6d8a779f3aedfba2c873be4decf112c699f9bf
sayadennis <sayarenedennis@gmail.com>
2024-08-05 15:47:26 -0500

    tissue category label: Normal -> CFN

--- exploration/visualize_normal_comparison_overlaps.py
+++ exploration/visualize_normal_comparison_overlaps.py
@@ -9,7 +9,7 @@ dv_din = "/projects/p30791/methylation/differential_variability/missMethylDiffVa
 dout = "/projects/p30791/methylation/plots"
 
 setnames = [
-    "Normal_vs_TU",
+    "CFN_vs_TU",
     "CUB_vs_TU",
     "OQ_vs_TU",
     "AN_vs_TU",
