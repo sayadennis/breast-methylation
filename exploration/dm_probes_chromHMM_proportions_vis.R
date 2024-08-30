185dd642f082c89ebc7989ba782ad1b67dd69038
sayadennis <sayarenedennis@gmail.com>
2024-08-30 17:19:21 -0500

    horizontal bar proportions of chromHMM cats in DM probes

---
+++ exploration/dm_probes_chromHMM_proportions_vis.R
@@ -0,0 +1,55 @@
+library(stringr)
+library(jsonlite)
+library(sesame)
+library(ggplot2)
+
+probe_dir <- "/projects/p30791/methylation/differential_methylation"
+dout <- probe_dir
+
+## Read the query probe sets
+probe_sets <- list(
+  hyper_refCFN_compCUB = readLines(paste0(probe_dir, "/probe_set_hyper_refCFN_compCUB.txt")),
+  hypo_refCFN_compCUB = readLines(paste0(probe_dir, "/probe_set_hypo_refCFN_compCUB.txt")),
+  hyper_refAN_compTU = readLines(paste0(probe_dir, "/probe_set_hyper_refAN_compTU.txt")),
+  hypo_refAN_compTU = readLines(paste0(probe_dir, "/probe_set_hypo_refAN_compTU.txt"))
+)
+
+## Download public Databases
+db_id <- "KYCG.EPIC.chromHMM.20211020" # we're only using this one here
+db <- KYCG_getDBs(db_id)
+
+##########################################
+#### Classify DM probes with chromHMM ####
+##########################################
+
+numbers <- as.numeric(sub("_.*", "", names(db)))
+sorted_names <- names(db)[order(numbers)]
+
+df <- data.frame(matrix(NA, nrow = length(sorted_names), ncol = 4))
+
+rownames(df) <- sorted_names
+colnames(df) <- c("CFNvsCUB.hyper", "CFNvsCUB.hypo", "ANvsTU.hyper", "ANvsTU.hypo")
+
+ref_list <- c("CFN", "AN")
+comp_list <- c("CUB", "TU")
+
+for (chromname in sorted_names) {
+  for (i in seq_along(ref_list)) {
+    ref <- ref_list[i]
+    comp <- comp_list[i]
+    for (trend in c("hyper", "hypo")) {
+      ct <- length(intersect(
+        db[[chromname]],
+        probe_sets[[paste0(trend, "_ref", ref, "_comp", comp)]]
+      ))
+      df[chromname, paste0(ref, "vs", comp, ".", trend)] <- ct
+    }
+  }
+}
+
+write.csv(
+  df,
+  file = paste0(dout, "/num_dm_probes_by_chromHMM.csv"),
+  quote = FALSE,
+  row.names = TRUE
+)
---
+++ exploration/dm_probes_chromHMM_proportions_vis.py
@@ -0,0 +1,70 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+input_fn = "/projects/p30791/methylation/differential_methylation/num_dm_probes_by_chromHMM.csv"
+dout = "/projects/p30791/methylation/plots/differential_methylation"
+
+cts = pd.read_csv(input_fn, index_col=0)
+
+totals = cts.sum(axis=0)
+pcts = 100 * cts.divide(totals)
+pcts_cum = pcts.cumsum(axis=0)
+
+color_mapping = {
+    "1_TssA": "red",
+    "2_TssAFlnk": "tomato",
+    "3_TxFlnk": "limegreen",
+    "4_Tx": "forestgreen",
+    "5_TxWk": "darkgreen",
+    "6_EnhG": "lightgreen",
+    "7_Enh": "gold",
+    "8_ZNF/Rpts": "mediumaquamarine",
+    "9_Het": "steelblue",
+    "10_TssBiv": "indianred",
+    "11_BivFlnk": "lightcoral",
+    "12_EnhBiv": "olivedrab",
+    "13_ReprPC": "darkgrey",
+    "14_ReprPCWk": "lightgrey",
+    "15_Quies": "whitesmoke",
+}
+
+fig, ax = plt.subplots(figsize=(10, 3.5))
+
+ax.invert_yaxis()
+
+for chromname in pcts.index:
+    ax.barh(
+        y=[0, 1, 3, 4],
+        width=pcts.loc[chromname].values,
+        left=pcts_cum.loc[chromname].values - pcts.loc[chromname].values,
+        height=0.5,
+        label=chromname,
+        color=color_mapping[chromname],
+        edgecolor="black",
+        linewidth=0.75,
+    )
+
+ax.spines["top"].set_visible(False)
+ax.spines["bottom"].set_visible(False)
+
+ax.set_xlim([0, 100])
+ax.set_yticks([0, 1, 3, 4])
+ax.set_yticklabels(
+    [
+        f"Hyper\n({cts.sum(axis=0).values[0]:,})",
+        f"Hypo\n({cts.sum(axis=0).values[1]:,})",
+        f"Hyper\n({cts.sum(axis=0).values[2]:,})",
+        f"Hypo\n({cts.sum(axis=0).values[3]:,})",
+    ]
+)
+ax.set_xlabel("Percentages (%)")
+ax.legend(ncols=1, bbox_to_anchor=(1.01, 0.5), loc="center left", fontsize="small")
+
+# Add row labels with genes
+fig.text(0.02, 0.78, "CFN vs CUB", rotation=90, va="center", ha="center", fontsize=12)
+fig.text(0.02, 0.30, "AN vs TU", rotation=90, va="center", ha="center", fontsize=12)
+
+plt.tight_layout()
+plt.subplots_adjust(left=0.10)
+fig.savefig(f"{dout}/dm_chromHMM_proportions_bar.png")
+plt.close()
