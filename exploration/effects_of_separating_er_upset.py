5a19805a5f228902209f3b119271138c1fd3c6ee
sayadennis <sayarenedennis@gmail.com>
2024-10-09 15:26:05 -0500

    write probe sets uniquely discovered in stratified comparisons

--- exploration/effects_of_separating_er_upset.py
+++ exploration/effects_of_separating_er_upset.py
@@ -21,9 +21,9 @@ dm_probesets = {
 # Read DM probe sets
 for trend, probe_dict in dm_probesets.items():
     for setname, file_pattern in {
-        "Overall": trend,
-        "ER+": f"{trend}_ER+",
-        "ER-": f"{trend}_ER-",
+        "Pooled all": trend,
+        "Stratified ER+": f"{trend}_ER+",
+        "Stratified ER-": f"{trend}_ER-",
     }.items():
         with open(
             f"{dm_din}/probe_set_{file_pattern}_{comparison}.txt", "r", encoding="utf-8"
@@ -63,3 +63,30 @@ upsetplot.plot(plot_data_dm["hypo"].value_counts(), sort_categories_by="input")
 plt.title("Hypo")
 plt.savefig(f"{dout}/upset_effects_of_separating_er_{comparison}_hypo.png")
 plt.close()
+
+
+## Exploratory ##
+
+# Get the sets that are uniquely discovered in stratified comparisons
+for trend in ["hyper", "hypo"]:
+    df = plot_data_dm[trend]
+    pos_only = df.iloc[
+        (df["Pooled all"].values is False)
+        & (df["Stratified ER+"].values is True)
+        & (df["Stratified ER-"].values is False),
+        :,
+    ].index
+    neg_only = df.iloc[
+        (df["Pooled all"].values is False)
+        & (df["Stratified ER+"].values is False)
+        & (df["Stratified ER-"].values is True),
+        :,
+    ].index
+    # write positive only
+    with open(f"/home/srd6051/OQ_AN_{trend}_ERpos_only.txt", "w") as f:
+        for item in pos_only:
+            f.write(f"{item}\n")
+    # write negative only
+    with open(f"/home/srd6051/OQ_AN_{trend}_ERneg_only.txt", "w") as f:
+        for item in neg_only:
+            f.write(f"{item}\n")
